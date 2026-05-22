a85b9457899890f606bd9c28d6375945110d2cb1
sagarparker <sagar8parker@gmail.com>
2026-05-22 22:44:16 +0530

    Update to the exp section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,33 +10,35 @@ interface ExperienceItem {
 const Experience: React.FC = () => {
   const experiences: ExperienceItem[] = [
     {
-      date: 'December 2024 - Present',
+      date: 'December 2024 - May 2026',
       title: 'Backend Software Engineer',
-      company: 'Blackfish Technologies (Marlin Protocol)',
+      company: 'Marlin Protocol (Blackfish technologies)',
       description: [
         'Engineered the Oyster Serverless CLI, an interactive CLI tool built in Rust, dramatically simplifying serverless deployments and improving developer workflows through automation.',
-        'Led R&D for Twitter Agent Service, an AI-powered solution built in Python that securely manages Twitter API credentials within a TEE, enabling verifiable tweets while preserving confidentiality.',
+        'Led R&D for Twitter Agent Service, an AI-powered solution built in Python & BrowserUse that securely manages Twitter API credentials within a TEE, enabling verifiable tweets while preserving confidentiality.',
+        'Built a pay-per-prompt AI chat platform and high-performance payment gateway using Rust, TS and x402, enabling AI agents to monetize APIs and LLM interactions through stablecoin micropayments without requiring credit cards.',
         'Spearheaded the research and end-to-end development of Oyster Persistent Storage using NFS, GoCryptfs, Docker, and Nix, enabling users to persist data across system reboots.',
-        'Implemented company-wide monitoring using a Prometheus backend (TypeScript) and Grafana dashboard, enabling real-time performance insights, proactive issue detection, and significantly improving system reliability and operational transparency.'
+        'Implemented company-wide monitoring using a Prometheus backend (TypeScript) and Grafana dashboards, along with live Telegram alerts for active issues, enabling real-time performance insights and proactive issue detection, which reduced issue resolution response time by 80% while improving system reliability.'
       ]
     },
     {
       date: 'August 2024 - October 2024',
-      title: 'Senior Software Engineer',
+      title: 'Delivery Software Engineer 3',
       company: 'Turing',
       description: [
+        'Collaborated with the PepsiCo USA team to develop dashboards delivering meaningful insights into Frito-Lay data through interactive charts and high-performance APIs.',
         'Engineered a high-performance back-end with Java Spring Boot and PostgreSQL, leveraging the MVC architecture to efficiently handle complex tasks and deliver high-throughput REST APIs.',
-        'Collaborated with the front-end team to build reusable React components, manage complex state using Redux and React Hooks, and handle API integration for processing large volumes of data.'
+        'Developed reusable React components, managed complex state using Redux and React Hooks, integrated APIs, and implemented caching to efficiently process and display large volumes of data.'
       ]
     },
     {
       date: 'December 2021 - June 2024',
-      title: 'Software Engineer',
-      company: 'Blackfish Technologies (Marlin Protocol)',
+      title: 'Full-Stack Software Engineer',
+      company: 'Marlin Protocol (Blackfish technologies)',
       description: [
         'Spearheaded the research and development of Oyster Serverless, a serverless platform to securely run code inside a TEE using AWS Nitro Enclaves, Docker, iptables, DNS proxy, and cgroups.',
-        'Significantly contributed to the R&D of Marlin Oyster, a TEE system based on AWS Nitro Enclaves for verifiable and secure computations.',
-        'Played a significant role in developing the client-side SDK (TypeScript) and a multi-threaded server-side zk-proof generator (Rust) for Kalypso, a decentralised system that enables protocols to delegate the process of zk-proof generation to a trustless system.',
+        'Significantly contributed to the research and development of Marlin Oyster a TEE system based on AWS nitro enclaves which allow computations to happen verifiably and securely.',
+        'Played a significant role in developing the client-side SDK (TypeScript) and a multi-threaded server-side zk-proof generator (Rust) for Kalypso, a decentralized system for trustless zk-proof generation.',
         'Successfully contributed to front-end development by integrating APIs (Axios/fetch) and smart contracts (web3.js, ethers.js), and developing over 5 responsive applications using React, Hooks, Zustand, CSS and Bootstrap, collectively locking over 200 million USD of TVL.'
       ]
     }
